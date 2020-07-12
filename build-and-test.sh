dfe8689ff12e88b50610c6bac051488d9174776d
Renoir Fortytwo <renoir42@yahoo.com>
2020-07-12 19:43:03 +0900

    Update build-and-test.sh

--- build-and-test.sh
+++ build-and-test.sh
@@ -9,7 +9,7 @@ cmake -DCMAKE_BUILD_TYPE=Release -DUSE_CUDA=OFF -DUSE_JULIA=OFF .. || exit 1
 make -j6 || exit 1
 ./TestThetis || exit 1
 popd
-zip thetis.zip build_release/libThetis.so build_release/TestThetis testData licenses LICENSE
+zip thetis.zip build_release/libThetis.so build_release/TestThetis testData/* licenses LICENSE
 echo pwd is $PWD
 ls -lh *.zip
 echo ls -lh /home/runner/work/Thetis/Thetis/thetis.zip
